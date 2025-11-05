42fb82939674e7ab0702af06528f26dccc36a68d
Marcos Franco <marcosdlfranco@gmail.com>
2025-11-05 00:23:57 -0600

    Instalamos la dependecia de desarrollo de gulp-sass que permite nos permitira conectar nuestro gulpfile con Sass adepmas de crear una funcion que sustituye el script que teniamos de sass en el package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,13 @@
-export function hola(done){
-    console.log('Hola desde gulpfile.js')
+import {src, dest} from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+const sass = gulpSass(dartSass)
+
+export function css(done){
+    src('src/scss/app.scss')//Ubicamos el archivo
+         .pipe(sass()) //aplica sass
+         .pipe(dest('build/css')) //Destino donde almacenamos el archivo
+
     done()
 }
