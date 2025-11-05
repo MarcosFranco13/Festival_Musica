119b91e92475a99cb57ee2ed7fe2846c4476889b
Marcos Franco <marcosdlfranco@gmail.com>
2025-11-05 11:52:50 -0600

    Creamos la funcion de dev en gulpfille para que realice la tarea de watch asi completamente sutituimos el script incial que teniamos de sass donde ejecutaba simultaneamte hasta que nosotros lo detuvieramos.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-import {src, dest} from 'gulp'
+import {src, dest, watch} from 'gulp'
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 
@@ -10,4 +10,8 @@ export function css(done){
          .pipe(dest('build/css')) //Destino donde almacenamos el archivo
 
     done()
+}
+
+export function dev(){
+    watch('src/scss/app.scss', css)//indicamos el archivo que este en observación y que funcion se ejecute cuando existan cambios
 }
