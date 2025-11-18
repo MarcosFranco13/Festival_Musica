511c6f698e2db0a7778acf871af153cbfc1f6eba
Marcos Franco <marcosdlfranco@gmail.com>
2025-11-18 12:01:57 -0600

    Generamos galeria en por medio de javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
-import {src, dest, watch} from 'gulp'
+import {src, dest, watch,series} from 'gulp'
+// series permite ejecutar una tarea y despues otra, por lo que se puede ejecutar multiples tareas
+//parallel permite arracanr al mismo tiempo todas la tareas
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 
 const sass = gulpSass(dartSass)
 
+export function js(done){
+    src('src/js/app.js')
+        .pipe(dest('build/js'))
+        
+    done()
+}
+
 export function css(done){
-    src('src/scss/app.scss')//Ubicamos el archivo
+    src('src/scss/app.scss', {sourcemaps:true})//Ubicamos el archivo
          .pipe(sass().on('error', sass.logError)) //aplica sass
-         .pipe(dest('build/css')) //Destino donde almacenamos el archivo
+         .pipe(dest('build/css', {sourcemaps: true})) //Destino donde almacenamos el archivo
 
     done()
 }
@@ -16,4 +25,7 @@ export function dev(){
     watch('src/scss/**/*.scss', css)//indicamos el archivo que este en observación y que funcion se ejecute cuando existan cambios
     //Cambiamos el nombre de app.scss por uno mas generico que nos permita encontrar mas archivos que tengan .scss
     //Entonces en src y scss **buscara todos las carpetas que esten dentro entonces * todos los archivos que tenga la extension .scss
+    watch('src/js/**/*.js', js)
 }
+
+export default series(js,css, dev)
---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function(){
+    crearGaleria()
+})
+
+function crearGaleria(){
+   const galeria = document.querySelector('.galeria-imagenes')
+   for(let i = 1; i<=16; i++){
+        const imagen = document.createElement('IMG');
+         imagen.src =`src/img/gallery/full/${i}.jpg`
+         imagen.alt = 'Imagen Galeria'
+         galeria.appendChild(imagen)
+   }
+}
