a1dd1fefee23fba939f292aea4402642ee7462f6
Marcos Franco <marcosdlfranco@gmail.com>
2025-11-18 22:32:14 -0600

    Creación de modal con javaScript y modificación con sass.

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,39 @@
-document.addEventListener('DOMContentLoaded', function(){
-    crearGaleria()
-})
+document.addEventListener("DOMContentLoaded", function () {
+  crearGaleria();
+});
 
-function crearGaleria(){
-    const CANTIDAD_IMAGENES = 16
-    const galeria = document.querySelector('.galeria-imagenes')
-   
+function crearGaleria() {
+  const CANTIDAD_IMAGENES = 16;
+  const galeria = document.querySelector(".galeria-imagenes");
 
-   for(let i = 1; i<=CANTIDAD_IMAGENES; i++){
-        const imagen = document.createElement('IMG');
-         imagen.src =`src/img/gallery/full/${i}.jpg`
-         imagen.alt = 'Imagen Galeria'
-         galeria.appendChild(imagen)
-   }
+  for (let i = 1; i <= CANTIDAD_IMAGENES; i++) {
+    const imagen = document.createElement("IMG");
+    imagen.src = `src/img/gallery/full/${i}.jpg`;
+    imagen.alt = "Imagen Galeria";
+
+    //Event Handler - Es el proceso de detectar y responder a una interacción del usuario
+    imagen.onclick = function(){
+        mostrarImagen(i)
+    }
+
+    galeria.appendChild(imagen);
+  }
+}
+
+function mostrarImagen(i){
+   //Generar Modal
+   const modal = document.createElement('DIV')
+   modal.classList.add('modal')
+   modal.onclick = cerrarModal
+
+   //Agregar al HTMl
+   const body = document.querySelector('body')
+   body.appendChild(modal)
+
+   console.log(modal)
+}
+
+function cerrarModal(){
+    const modal = document.querySelector('.modal')
+    modal?.remove();//modal? es equivalente aun if para validar si existe o no.
 }
