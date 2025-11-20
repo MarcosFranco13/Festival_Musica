c90f427fb01febfe7e8af54aac29548a6c5e682a
Marcos Franco <marcosdlfranco@gmail.com>
2025-11-20 16:01:53 -0600

    Ajustamos estilos de modal permitiendo que la imagen que eligamos aparca y desvanisca con keyframes haciendo efectos y fijando la pantalla agregando una clases en body por js overflow hidden para posteriomente eliminarla cuando se cierre el modal.

--- src/js/app.js
+++ src/js/app.js
@@ -12,28 +12,46 @@ function crearGaleria() {
     imagen.alt = "Imagen Galeria";
 
     //Event Handler - Es el proceso de detectar y responder a una interacción del usuario
-    imagen.onclick = function(){
-        mostrarImagen(i)
-    }
+    imagen.onclick = function () {
+      mostrarImagen(i);
+    };
 
     galeria.appendChild(imagen);
   }
 }
 
-function mostrarImagen(i){
-   //Generar Modal
-   const modal = document.createElement('DIV')
-   modal.classList.add('modal')
-   modal.onclick = cerrarModal
+function mostrarImagen(i) {
+  const imagen = document.createElement("IMG");
+  imagen.src = `src/img/gallery/full/${i}.jpg`;
+  imagen.alt = "Imagen Galeria";
 
-   //Agregar al HTMl
-   const body = document.querySelector('body')
-   body.appendChild(modal)
+  //Generar Modal
+  const modal = document.createElement("DIV");
+  modal.classList.add("modal");
+  modal.onclick = cerrarModal;
 
-   console.log(modal)
+  modal.appendChild(imagen);
+
+  //Agregar al HTMl
+  const body = document.querySelector("body");
+  body.classList.add("overflow-hidden");
+  body.appendChild(modal);
+
+  console.log(modal);
 }
 
-function cerrarModal(){
-    const modal = document.querySelector('.modal')
-    modal?.remove();//modal? es equivalente aun if para validar si existe o no.
+function cerrarModal() {
+  const modal = document.querySelector(".modal");
+  modal.classList.add("fade-out");
+
+  // modal.addEventListener('animationend', () => {
+  //     modal.remove();
+  // }, { once: true });
+
+  setTimeout(() => {
+    modal?.remove(); //modal? es equivalente aun if para validar si existe o no.
+
+    const body = document.querySelector("body");
+    body.classList.remove("overflow-hidden");
+  }, 500);
 }
