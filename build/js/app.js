d81f0520decb19e65cf31b53204055eec1b094df
Marcos Franco <marcosdlfranco@gmail.com>
2025-11-20 22:38:11 -0600

    Agregamos efecto scroll con quitando el default con preventdefault y utilizando scollintoview con behavior smooth.

--- build/js/app.js
+++ build/js/app.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   navegacionFija();
   crearGaleria();
   resaltarEnlace();
+  scrollNav();
 });
 
 function navegacionFija(){
@@ -102,6 +103,22 @@ function resaltarEnlace(){
           link.classList.add('active')
       }
     })
-
+ 
   })
+}
+
+function scrollNav(){
+  const navLinks = document.querySelectorAll('.navegacion-principal a')
+
+  navLinks.forEach(link =>{
+    link.addEventListener('click', e =>{
+      e.preventDefault()
+      const sectionScroll = e.target.getAttribute('href')
+      const section = document.querySelector(sectionScroll)
+
+      section.scrollIntoView({behavior: 'smooth'})//Crea el efecto de scroll suavizado 
+
+    })
+  })
+
 }
