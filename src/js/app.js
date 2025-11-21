e365e93528d80c4e6472fd9edf9f9b0e4b7e8814
Marcos Franco <marcosdlfranco@gmail.com>
2025-11-20 19:40:16 -0600

    Resaltar sección actual con scroll js

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   navegacionFija();
   crearGaleria();
+  resaltarEnlace();
 });
 
 function navegacionFija(){
@@ -78,4 +79,29 @@ function cerrarModal() {
     const body = document.querySelector("body");
     body.classList.remove("overflow-hidden");
   }, 500);
+}
+
+function resaltarEnlace(){
+  document.addEventListener('scroll', function(){
+    const sections = document.querySelectorAll('section')
+    const navLinks = document.querySelectorAll('.navegacion-principal a')
+
+    let actual = '';
+    sections.forEach( section => {
+      const sectionTop = section.offsetTop
+      const seccionHeight = section.clientHeight
+
+      if (window.scrollY >= (sectionTop - seccionHeight / 3)){
+          actual = section.id
+      }
+    })
+
+    navLinks.forEach(link =>{
+      link.classList.remove('active')
+      if(link.getAttribute('href') === '#' + actual){
+          link.classList.add('active')
+      }
+    })
+
+  })
 }
