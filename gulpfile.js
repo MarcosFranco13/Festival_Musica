8b17be8a360a6babb6e508137265ed0edd1bc19d
Marcos Franco <marcosdlfranco@gmail.com>
2025-11-25 19:21:32 -0600

    Instalamos la dependecia de sharp que nos permite reducir las imagenes o modificarlas y las añadimos en gulfile con junto a un codigo en node.js que permite realizar dicha tarea

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,6 @@
+import path from 'path'
+import fs from 'fs' 
+
 import {src, dest, watch,series} from 'gulp'
 // series permite ejecutar una tarea y despues otra, por lo que se puede ejecutar multiples tareas
 //parallel permite arracanr al mismo tiempo todas la tareas
@@ -7,6 +10,7 @@ import gulpSass from 'gulp-sass'
 const sass = gulpSass(dartSass)
 
 import terser from 'gulp-terser'
+import sharp from 'sharp'
 
 export function js(done){
     src('src/js/app.js')
@@ -24,6 +28,34 @@ export function css(done){
     done()
 }
 
+export async function crop(done) {
+    const inputFolder = 'src/img/gallery/full'
+    const outputFolder = 'src/img/gallery/thumb';
+    const width = 250;
+    const height = 180;
+    if (!fs.existsSync(outputFolder)) {
+        fs.mkdirSync(outputFolder, { recursive: true })
+    }
+    const images = fs.readdirSync(inputFolder).filter(file => {
+        return /\.(jpg)$/i.test(path.extname(file));
+    });
+    try {
+        images.forEach(file => {
+            const inputFile = path.join(inputFolder, file)
+            const outputFile = path.join(outputFolder, file)
+            sharp(inputFile) 
+                .resize(width, height, {
+                    position: 'centre'
+                })
+                .toFile(outputFile)
+        });
+
+        done()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 export function dev(){
     watch('src/scss/**/*.scss', css)//indicamos el archivo que este en observación y que funcion se ejecute cuando existan cambios
     //Cambiamos el nombre de app.scss por uno mas generico que nos permita encontrar mas archivos que tengan .scss
@@ -31,4 +63,4 @@ export function dev(){
     watch('src/js/**/*.js', js)
 }
 
-export default series(js,css, dev)
+export default series(crop,js,css, dev)
